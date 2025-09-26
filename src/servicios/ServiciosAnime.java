4e0cfb566beeab6b1e79116fff6961d34332e3ae
CabbaGG2 <jcamaradepablos@danielcastelao.org>
2025-09-26 14:35:46 +0200

    comienzo de creación de metodos en la clase ServiciosAnime para consultar datos en la BBDD.

--- src/servicios/ServiciosAnime.java
+++ src/servicios/ServiciosAnime.java
@@ -5,11 +5,13 @@ import java.sql.Connection;
 
 public class ServiciosAnime {
 
- public class insert(String url, String usuario, String contrasinal) {
+ /*public class insert(String url, String usuario, String contrasinal) {
             // Cambia estos valores por los de tu base de datos
            Connection connection = dbConnection.conectar();
            try( )
             connection.excutePreparedStatement("SELECT * FROM anime");
 
     }
+
+  */
 }
